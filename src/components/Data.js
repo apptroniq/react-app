bc82beade4338de193e416845a29a649a6615373
Asfund <asfund@asfund-mac.local>
2025-04-11 12:40:41 +0500

    async axios

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,32 +1,71 @@
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
-export default function Data({ }) {
+export default function Data() {
     const [users, setUsers] = useState([]);
     const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        console.log("calling API *******");
-        axios.get('https://jsonplaceholder.typicode.com/users')
-      .then(response => {
-        setUsers(response.data);
-      })
-      .catch(error => {
-        setError('Error fetching data');
-        console.error(error);
-      });
+        const fetchData = async () => {
+            console.log("calling API *******");
+            try {
+                setLoading(true);
+                const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+                setUsers(response.data);
+                setLoading(false);
 
+                setTimeout(() => {
+                    response.data[1].name = "Muhammad Asfund Yar";
+                    setUsers(response.data);
+                }, 5000);
+            } catch (err) {
+                setError('Error fetching data');
+                console.error(err);
+                setLoading(false);
+            }
+        };
+        fetchData();
     }, []);
+
+    const changeName = (i) => {
+        console.log("Changing name");
+        setUsers((users) => {
+            // users[2].name = "Muhammad Rayan";
+            // return users;
+
+            const buUsers = [...users];
+            buUsers[i].name = "Muhammad Rayan";
+
+            // const updatedKeys = { name: "Muhammad Rayan" };
+            // buUsers[i] = {
+            //     ...buUsers[i],
+            //     ...updatedKeys
+            // };
+
+            return buUsers;
+        });
+    }
     
     return (
         <div>
             <h2>User List</h2>
+
             {error && <p>{error}</p>}
-            <ul>
-                {users.map(user => (
-                <li key={user.id}>{user.name}</li>
-                ))}
-            </ul>
+
+            {loading && <p>Please wait. We are fetching records</p>}
+            { !loading &&
+                <div>
+                    <ul>
+                        {
+                            users.map(user => (
+                                <li key={user.id}>{user.name}</li>
+                            ))
+                        }
+                    </ul>
+                    <button className="btn btn-primary btn-sm" onClick={() => changeName(4)}>Change Name</button>
+                </div>
+            }
         </div>
     )
 }
